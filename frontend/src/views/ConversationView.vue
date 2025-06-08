c789a1f8774a68f293e66e3a771ab2651093ac21
Matej Prokopic <32707168+Qsuluqod@users.noreply.github.com>
2025-06-08 18:55:27 +0200

    style: unify button design

--- frontend/src/views/ConversationView.vue
+++ frontend/src/views/ConversationView.vue
@@ -5,7 +5,7 @@
       <div class="fixed top-24 left-6 z-30 flex items-center gap-4" style="min-width: 260px;">
         <button
           @click="goBack"
-          class="flex items-center gap-2 px-4 py-2 rounded-lg bg-gradient-to-r from-primary-from to-primary-to text-white font-semibold shadow hover:from-primary-to hover:to-primary-from transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-primary-from"
+          class="btn-primary flex items-center gap-2"
         >
           <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd" d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z" clip-rule="evenodd" />
