be8d51d696525ef116cb182663a03d926eec7761
Matěj Prokopič <prokopicmatej@gmail.com>
2025-06-08 21:52:34 +0200

    Add data folder

---
+++ backend/Data/KorpusDbContext.cs
@@ -0,0 +1,50 @@
+using Microsoft.EntityFrameworkCore;
+using ChatGPTcorpus.Models;
+
+namespace ChatGPTcorpus.Data
+{
+    public class KorpusDbContext : DbContext
+    {
+        public KorpusDbContext(DbContextOptions<KorpusDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<DbConversation> Conversations { get; set; }
+        public DbSet<DbMessage> Messages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<DbConversation>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Title).IsRequired();
+                entity.Property(e => e.Author).IsRequired();
+                entity.Property(e => e.CreateTime).IsRequired();
+                entity.Property(e => e.UpdateTime).IsRequired();
+                
+                // Configure the one-to-many relationship with messages
+                entity.HasMany(e => e.Messages)
+                      .WithOne(e => e.Conversation)
+                      .HasForeignKey(e => e.ConversationId)
+                      .OnDelete(DeleteBehavior.Cascade);
+            });
+
+            modelBuilder.Entity<DbMessage>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Author).IsRequired();
+                entity.Property(e => e.Content).IsRequired();
+                entity.Property(e => e.CreateTime).IsRequired();
+                entity.Property(e => e.Status).IsRequired();
+                
+                // Configure the relationship with conversation
+                entity.HasOne(e => e.Conversation)
+                      .WithMany(e => e.Messages)
+                      .HasForeignKey(e => e.ConversationId)
+                      .OnDelete(DeleteBehavior.Cascade);
+            });
+        }
+    }
+} 
